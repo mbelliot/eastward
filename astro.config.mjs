65989bde434288037fc4dc4ec2a032ee8333b375
Elliot Li <yaogwai@gmail.com>
2026-06-16 12:47:48 +0800

    ignore sanity studio build on prod

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,45 +1,55 @@
 // @ts-check
 
-import sanity from "@sanity/astro"
-import tailwindcss from "@tailwindcss/vite"
-import { defineConfig, fontProviders } from "astro/config"
+import sanity from '@sanity/astro'
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig, fontProviders } from 'astro/config'
 
-import react from "@astrojs/react";
+import react from '@astrojs/react'
 
-import icon from "astro-icon";
+import icon from 'astro-icon'
+
+const isProd = process.env.NODE_ENV === 'production'
 
 export default defineConfig({
-    fonts: [
-        {
-            provider: fontProviders.google(),
-            name: "Jost",
-            cssVariable: "--font-jost",
-            weights: [300, 400, 500],
-            styles: ["normal", "italic"],
-        },
-        {
-            provider: fontProviders.google(),
-            name: "Tenor Sans",
-            cssVariable: "--font-tenor",
-            weights: [300, 400],
-            styles: ["normal"],
-        },
-    ],
-
-    vite: {
-        plugins: [tailwindcss()],
-        server: {
-            watch: {
-                ignored: ["**/sanity.types.ts"],
-            },
-        },
+  fonts: [
+    {
+      provider: fontProviders.google(),
+      name: 'Jost',
+      cssVariable: '--font-jost',
+      weights: [300, 400, 500],
+      styles: ['normal', 'italic'],
     },
+    {
+      provider: fontProviders.google(),
+      name: 'Tenor Sans',
+      cssVariable: '--font-tenor',
+      weights: [300, 400],
+      styles: ['normal'],
+    },
+  ],
 
-    integrations: [sanity({
-        projectId: "4cflhpla",
-        dataset: "production",
-        useCdn: false,
-        apiVersion: "2026-06-03",
-        studioBasePath: "/cms",
-        }), react(), icon()],
+  vite: {
+    plugins: [tailwindcss()],
+    server: {
+      watch: {
+        ignored: ['**/sanity.types.ts'],
+      },
+    },
+  },
+
+  integrations: [
+    ...(isProd
+      ? []
+      : [
+          sanity({
+            projectId: '4cflhpla',
+            dataset: 'production',
+            useCdn: false,
+            apiVersion: '2026-06-03',
+            studioBasePath: '/cms',
+          }),
+          react(),
+        ]),
+    icon(),
+  ],
 })
