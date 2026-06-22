0d880480d5eba818fa704c6ff704893602a465fc
Elliot Li <yaogwai@gmail.com>
2026-06-22 15:21:10 +0800

    update deps

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,25 +1,29 @@
-import { defineCliConfig } from "sanity/cli"
+import { defineCliConfig } from 'sanity/cli'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineCliConfig({
-	api: {
-		projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID || "",
-		dataset: import.meta.env.PUBLIC_SANITY_DATASET || "",
-	},
-	schemaExtraction: {
-		enabled: true,
-		enforceRequiredFields: true,
-		watchPatterns: ["./src/sanity/schemaTypes/**/*.ts"],
-	},
-	typegen: {
-		enabled: true,
-		path: "./src/**/*.{ts,tsx,astro}",
-		generates: "./sanity.types.ts",
-	},
-	deployment: {
-		/**
-		 * Enable auto-updates for studios.
-		 * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
-		 */
-		autoUpdates: true,
-	},
+  api: {
+    projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID || '4cflhpla',
+    dataset: import.meta.env.PUBLIC_SANITY_DATASET || 'production',
+  },
+  schemaExtraction: {
+    enabled: true,
+    enforceRequiredFields: true,
+    watchPatterns: ['./src/sanity/schemaTypes/**/*.ts'],
+  },
+  typegen: {
+    enabled: true,
+    path: './src/**/*.{ts,tsx,astro}',
+    generates: './sanity.types.ts',
+  },
+  deployment: {
+    /**
+     * Enable auto-updates for studios.
+     * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
+     */
+    autoUpdates: true,
+  },
+  vite: (config) => {
+    return { ...config, plugins: [tsconfigPaths()] }
+  },
 })
--- sanity.config.ts
+++ sanity.config.ts
@@ -1,11 +1,11 @@
-import { visionTool } from "@sanity/vision"
-import { defineConfig } from "sanity"
-import { structureTool } from "sanity/structure"
-import { schema } from "~/sanity/schemaTypes"
+import { visionTool } from '@sanity/vision'
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
+import { schema } from '~/sanity/schemaTypes'
 
 export default defineConfig({
-	projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID || "",
-	dataset: import.meta.env.PUBLIC_SANITY_DATASET || "",
-	plugins: [structureTool(), visionTool()],
-	schema,
+  projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID || '4cflhpla',
+  dataset: import.meta.env.PUBLIC_SANITY_DATASET || 'production',
+  plugins: [structureTool(), visionTool()],
+  schema,
 })
